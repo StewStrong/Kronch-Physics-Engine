564009c42af335706ecd9eb8664b7eb884843f50
Tri0de <best108@uw.edu>
2021-03-26 06:36:51 -0700

    Added Edge-Edge collisions to BoxBoxCollider

--- src/main/kotlin/org/valkyrienskies/kronch/collision/colliders/BoxBoxCollider.kt
+++ src/main/kotlin/org/valkyrienskies/kronch/collision/colliders/BoxBoxCollider.kt
@@ -1,6 +1,7 @@
 package org.valkyrienskies.kronch.collision.colliders
 
 import org.joml.Vector3d
+import org.joml.Vector3dc
 import org.joml.primitives.AABBdc
 import org.valkyrienskies.kronch.Pose
 import org.valkyrienskies.kronch.collision.CollisionPair
@@ -10,6 +11,9 @@ import org.valkyrienskies.kronch.collision.CollisionResultc
 import org.valkyrienskies.kronch.collision.NotCollidingException
 import org.valkyrienskies.kronch.collision.shapes.BoxCollisionShape
 import org.valkyrienskies.kronch.collision.shapes.boxPoints
+import org.valkyrienskies.kronch.getQuatAxis0
+import org.valkyrienskies.kronch.getQuatAxis1
+import org.valkyrienskies.kronch.getQuatAxis2
 import kotlin.math.abs
 import kotlin.math.min
 
@@ -20,6 +24,7 @@ object BoxBoxCollider : Collider<BoxCollisionShape, BoxCollisionShape> {
     ): CollisionResultc {
         val collisionPairs: MutableList<CollisionPairc> = ArrayList()
 
+        // Vertex collision
         collideCubes(
             firstBodyCollisionShape, firstBodyPose, secondBodyCollisionShape, secondBodyPose
         ) { firstCollisionPoint: Vector3d, secondCollisionPoint: Vector3d, collisionNormal: Vector3d ->
@@ -32,7 +37,135 @@ object BoxBoxCollider : Collider<BoxCollisionShape, BoxCollisionShape> {
             collisionPairs.add(CollisionPair(secondCollisionPoint, firstCollisionPoint, collisionNormal.mul(-1.0)))
         }
 
+        // SAT Collision
+        run {
+            val normals: MutableList<Vector3dc?> = ArrayList()
+            normals.add(getQuatAxis0(firstBodyPose.q))
+            normals.add(getQuatAxis1(firstBodyPose.q))
+            normals.add(getQuatAxis2(firstBodyPose.q))
+            normals.add(getQuatAxis0(secondBodyPose.q))
+            normals.add(getQuatAxis1(secondBodyPose.q))
+            normals.add(getQuatAxis2(secondBodyPose.q))
+            for (i in 0..2) {
+                for (j in 3..5) {
+                    val crossProduct = normals[i]!!.cross(normals[j], Vector3d())
+                    if (crossProduct.lengthSquared() > 1e-6) {
+                        crossProduct.normalize()
+                        normals.add(crossProduct)
+                    } else {
+                        // Normal not valid
+                        normals.add(null)
+                    }
+                }
+            }
+
+            val temp1 = CollisionRange.create()
+            val temp2 = CollisionRange.create()
+
+            var minResponse = Double.MAX_VALUE
+            var minNormalIndex = -1
+            var flipNormal = false
+
+            for (i in 0 until normals.size) {
+                val normal = normals[i] ?: continue // Skip null normals
+                val firstBodyRange =
+                    BoxCollisionShape.projectBoxAlongAxis(firstBodyCollisionShape, firstBodyPose, normal, temp1)
+                val secondBodyRange =
+                    BoxCollisionShape.projectBoxAlongAxis(secondBodyCollisionShape, secondBodyPose, normal, temp2)
+                val response = CollisionRange.computeCollisionResponse(firstBodyRange, secondBodyRange)
+                if (response == 0.0) {
+                    // Not colliding
+                    return CollisionResult(listOf())
+                }
+                if (abs(response) < minResponse) {
+                    minResponse = abs(response)
+                    flipNormal = response > 0
+                    minNormalIndex = i
+                }
+            }
+
+            if (minNormalIndex < 6) {
+                // Face collision
+            } else {
+                // Edge collision
+                // val body0EdgeNormal: Vector3dc = normals[(minNormalIndex - 5) / 3]!!
+                // val body1EdgeNormal: Vector3dc = normals[((minNormalIndex - 5) % 3) + 3]!!
+                val collisionNormal: Vector3dc = normals[minNormalIndex]!!
+
+                val body0Support0 = Vector3d()
+                val body0Support1 = Vector3d()
+                val body1Support0 = Vector3d()
+                val body1Support1 = Vector3d()
+
+                setSupportVectors(
+                    firstBodyCollisionShape, firstBodyPose, collisionNormal, flipNormal xor true, body0Support0,
+                    body0Support1
+                )
+                setSupportVectors(
+                    secondBodyCollisionShape, secondBodyPose, collisionNormal, flipNormal xor false, body1Support0,
+                    body1Support1
+                )
+
+                val body0SupportDirection = body0Support1.sub(body0Support0, Vector3d())
+                val body1SupportDirection = body1Support1.sub(body1Support0, Vector3d())
+
+                // Collision between 2 edges is at the closest points on each line
+                // Implementation based off of Bullet physics https://github.com/bulletphysics/bullet3/blob/afa4fb54505fd071103b8e2e8793c38fd40f6fb6/src/BulletCollision/CollisionDispatch/btBoxBoxDetector.cpp#L83-L107
+                val p = body1Support0.sub(body0Support0, Vector3d())
+                val uaub = body0SupportDirection.dot(body1SupportDirection)
+                val q1 = body0SupportDirection.dot(p)
+                val q2 = -body1SupportDirection.dot(p)
+                val d = 1.0 / (1 - uaub * uaub)
+
+                if (abs(d) > 1e4) {
+                    // Give up
+                    return CollisionResult(listOf())
+                }
+
+                val alpha = (q1 + uaub * q2) * d
+                val beta = (uaub * q1 + q2) * d
+
+                val body0CollisionPoint = Vector3d(body0Support0).fma(alpha, body0SupportDirection)
+                val body1CollisionPoint = Vector3d(body1Support0).fma(beta, body1SupportDirection)
+
+                return CollisionResult(listOf(CollisionPair(body0CollisionPoint, body1CollisionPoint, collisionNormal)))
+            }
+        }
+
+
         return CollisionResult(collisionPairs)
+    }
+
+    private fun setSupportVectors(
+        boxCollisionShape: BoxCollisionShape, boxPose: Pose, collisionNormal: Vector3dc, invertNormal: Boolean,
+        support0: Vector3d, support1: Vector3d
+    ) {
+        var smallestProjection = Double.MAX_VALUE
+
+        for (box0Point in boxCollisionShape.aabb.boxPoints()) {
+            boxPose.transform(box0Point)
+            val alongAxis = if (invertNormal) -collisionNormal.dot(box0Point) else collisionNormal.dot(box0Point)
+            smallestProjection = min(smallestProjection, alongAxis)
+        }
+
+        var setSupport0 = false
+
+        for (box0Point in boxCollisionShape.aabb.boxPoints()) {
+            boxPose.transform(box0Point)
+            val alongAxis = if (invertNormal) -collisionNormal.dot(box0Point) else collisionNormal.dot(box0Point)
+            if (equals(alongAxis, smallestProjection)) {
+                if (!setSupport0) {
+                    support0.set(box0Point)
+                    setSupport0 = true
+                } else {
+                    support1.set(box0Point)
+                }
+            }
+        }
+    }
+
+    fun equals(a: Double, b: Double, EPSILON: Double = 1e-10): Boolean {
+        return abs(a - b) < EPSILON
     }
 
     private inline fun collideCubes(
